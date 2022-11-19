eb1e333128c87097df19ccff6e8db7a3ce23c12c
mawit <jackheaven023@gmail.com>
2022-11-19 17:27:38 +0100

    fixed some linting issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import useStore from "../store/useStore";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	const [client_width, set_client_width] = useState<String | null | Number>(null);
+	const [client_width, set_client_width] = useState<string | null | number>(null);
     const {authState, setAuthState} = useStore()
 	let timeout = useRef<any>();
 
@@ -15,7 +15,7 @@ export default function Home() {
 			set_client_width(null);
 		}, 2000);
 	}
-    function setIt(e : Event){
+    function setIt(){
         setAuthState(!authState)
         setAuthState(!authState)
     }
@@ -29,7 +29,7 @@ export default function Home() {
 				<p className="font-semibold max-[644px]:text-blue-500">
 					this is a platform in which Bakwerians can meet and share and help each other.
 				</p>
-				{client_width && <p>the width of this screen is: {client_width}</p>}
+				{client_width && <div>the width of this screen is: {client_width}</div>}
 				<button className="btn-blue" onClick={display_screen_width}>
 					click on me to see your screen width
 				</button>
