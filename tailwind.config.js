55930e4ceceead691443e07d8f49826eafcac514
mawit <jackheaven023@gmail.com>
2022-11-21 23:36:29 +0100

    enhanced the component shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
             "text_input_shadow" : "0px 1px 5px 0.5px rgba(0, 0, 0, 0.03)",
             "comp" :"0px 1px 5px 1px rgba(0, 0, 0, 0.09),0px 0px 0px rgba(0, 0, 0, 0.09)",
             "icon" : "0px 12px 5px rgba(0, 0, 0, 0.01),0px 7px 4px rgba(0, 0, 0, 0.05),0px 3px 3px rgba(0, 0, 0, 0.09),0px 1px 2px rgba(0, 0, 0, 0.1),0px 0px 0px rgba(0, 0, 0, 0.1)",
-            "comp_lg" : "14px 10px 11px rgba(0, 0, 0, 0.05), 2px 2px 4px rgba(0, 0, 0, 0.1)" ,
+            "comp_lg" : "-2px -2px 4px rgba(0, 0, 0, 0.08), 14px 10px 11px rgba(0, 0, 0, 0.05), 2px 2px 4px rgba(0, 0, 0, 0.1)" ,
             "outer_comp" : " 0px 48px 48px rgba(0, 0, 0, 0.21), 0px 12px 14px 3px rgba(0, 0, 0, 0.25), 0px 0px 0px rgba(0, 0, 0, 0.25)" 
         }
     },
