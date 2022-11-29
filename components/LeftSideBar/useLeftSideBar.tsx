d70382d8a47d16adb4987cef36763870b1a8e1d3
mawit <jackheaven023@gmail.com>
2022-11-29 08:20:54 +0100

    in the leftSideBar, the page checkers were still checking for each pages including the '/' sign at the prefix. which was incorrect as the '/' prefixe was removed from all Urls. so removing them form the page checkers fixed the issue

--- components/LeftSideBar/useLeftSideBar.tsx
+++ components/LeftSideBar/useLeftSideBar.tsx
@@ -11,31 +11,31 @@ export default function useLeftSideBar(){
     return { page , isHome , isJobs , isBusiness , isMarketPlace , isRealEstate , isCulturalGroups , isLitumbaHub , navigate}
 
     function isHome(){
-        const className : string = page.includes(ROUTES.home) ? "page-option-selected" : "page-option"
+        const className : string = page.includes(ROUTES.home.replace("/","")) ? "page-option-selected" : "page-option"
         return className
     }
     function isJobs(){
-        const className : string = page.includes(ROUTES.jobs.index) ? "page-option-selected" : "page-option"
+        const className : string = page.includes(ROUTES.jobs.index.replace("/","")) ? "page-option-selected" : "page-option"
         return className
     }
     function isBusiness(){
-        const className : string = page.includes(ROUTES.businesses.index) ? "page-option-selected" : "page-option"
+        const className : string = page.includes(ROUTES.businesses.index.replace("/","")) ? "page-option-selected" : "page-option"
         return className
     }
     function isMarketPlace(){
-        const className : string = page.includes(ROUTES.market_place.products.index) || page.includes(ROUTES.market_place.services.index) ? "page-option-selected" : "page-option"
+        const className : string = page.includes(ROUTES.market_place.products.index.replace("/","")) || page.includes(ROUTES.market_place.services.index.replace("/","")) ? "page-option-selected" : "page-option"
         return className
     }
     function isRealEstate(){
-        const className : string = page.includes(ROUTES.real_estate.index) ? "page-option-selected" : "page-option"
+        const className : string = page.includes(ROUTES.real_estate.index.replace("/","")) ? "page-option-selected" : "page-option"
         return className
     }
     function isCulturalGroups(){
-        const className : string = page.includes(ROUTES.cultural_groups.index) ? "page-option-selected" : "page-option"
+        const className : string = page.includes(ROUTES.cultural_groups.index.replace("/","")) ? "page-option-selected" : "page-option"
         return className
     }
     function isLitumbaHub(){
-        const className : string = page.includes(ROUTES.litumba_hub.index) ? "page-option-selected" : "page-option"
+        const className : string = page.includes(ROUTES.litumba_hub.index.replace("/","")) ? "page-option-selected" : "page-option"
         return className
     }
 }
